f7cfef71c7056793300c0d0f8ff81c998f1c028e
JhonRamosV <86128581+JhonRamosV@users.noreply.github.com>
2021-10-29 17:34:18 -0500

    creando primera parte del taller 2

---
+++ js/main.js
@@ -0,0 +1,52 @@
+/*primer ejercicio*/
+/*var horas = prompt ("ingrese Horas trabajadas")
+var valor = 5
+var minutos = 60
+
+horas = parseInt(horas)
+
+alert("las horas trabajadas son  " + horas + ", el pago por sus horas trabajadas es  " + horas * valor +  "  sus minutos trabajados son  "  + horas * minutos);*/
+
+
+/*segundo ejercicio*/
+
+/*var dineroCo = prompt ("ingrese cuantos dinero tiene")
+var peso = 3760.00
+var dolar = 1 
+
+dineroCo = parseInt(dineroCo)
+
+alert (" su cantidad de dinero que tiene es " + dineroCo + " la conversion de dolares a pesos es " + dineroCo / peso + " son  dolares  ")*/
+
+
+/*tercdr ejercicio*/
+
+/*var notaf = 70
+var pro = 3
+var nota1 = prompt ("ingrese nota uno");
+var nota2 = prompt ("ingrese nota 2");
+var nota3 = prompt ("ingrese nota 3");
+
+
+nota1 = parseInt(nota1);
+nota2 = parseInt(nota2);
+nota3 = parseInt(nota3);
+
+
+
+if((nota1+nota2+nota3) >= notaf){
+    alert("usted aprobo" + "su nota es  "  + (nota1 + nota2 + nota3)  + "su promedio es "  +  (nota1 + nota2 + nota3)/3);
+}else{
+    alert("usted no aprobo" + "su nota es  "  + (nota1 + nota2 + nota3)  + "su promedio es "  +  (nota1 + nota2 + nota3)/3);
+}*/
+
+/* cuarto ejercicio*/
+
+var nombre = prompt("ingrese su nombre");
+var meses = prompt("ingrese meses que trabajo");
+var calculo = 100
+var cantidadmes = 12
+
+meses = parseInt(meses);
+
+alert ("  hola  "  +  nombre  +  "  usted trabajo  "  +  meses  +  "  meses  "  +  "  su porcentaje trabajado en meses al año fue de  "  +  (calculo/cantidadmes)*meses  +  "%");
